2de572b5885a8d018db8fd1b261cf511205bb88c
japinl <japinli@hotmail.com>
2016-12-14 20:19:51 +0800

    完善安装脚步

--- install.sh
+++ install.sh
@@ -1,27 +1,83 @@
 #!/bin/bash
 
-DIR=`pwd`
-VIMRC=${DIR}/.vimrc
+function read_yes_no()
+{
+    printf "[Y/N]: "
+    read yesno
+    while [[ "${yesno}" != "Y" && "${yesno}" != "N" ]]
+    do
+        printf "Please enter [Y/N]: "
+        read yesno
+    done
+    if [[ "${yesno}" == "Y" ]]; then
+        return 0
+    fi
+    return 1
+}
 
-USER_VIMRC=$HOME/.vimrc
+function check_vim_executable()
+{
+    printf "Checking vim ... "
+    if which vim >/dev/null 2>&1; then
+        echo "OK"
+        return 0
+    fi
+    echo "ERR"
+    return 1
+}
 
 function install_vimrc()
 {
-    if [ -L ${USER_VIMRC} ]; then
-	OLD_VIMRC_LINK=`readlink ~/.vimrc`
-	if [ "${VIMRC}" = "${OLD_VIMRC_LINK}" ]; then
-	    return
-	fi
-	echo "relink your vimrc to ${VIMRC}"
-    elif [ -f ${USER_VIMRC} ]; then
-	echo "backup your .vimrc to vimrc.bak in your home directory"
-	mv ${USER_VIMRC} ${USER_VIMRC}.bak
-    fi
+    vimrc=.vimrc
+    new_vimrc=`pwd`/${vimrc}
+    
+    if [ -L ${HOME}/${vimrc} ]; then
+        link=`readlink ${HOME}/${vimrc}`
 
-    ln -s ${VIMRC} ${USER_VIMRC}
+        if [[ "${link}" == "${new_vimrc}" ]]; then
+            return 0
+        fi
+
+        printf "Relink your vim config? "
+        if read_yes_no ; then
+            echo "Relink your vim config to ${new_vimrc}"
+            ln -f -s ${new_vimrc} ${HOME}/${vimrc}
+            return $?
+        fi
+        
+    elif [ -f ${HOME}/{vimrc} ]; then
+        printf "You already have vim config file, relace it? "
+        if read_yes_no ; then
+            echo "Backup your tmux config to ${HOME}/vimrc.bak"
+            mv ${HOME}/${vimrc} ${HOME}/vimrc.bak && ln -s ${new_vimrc} ${HOME}/${vimrc}
+            return $?
+        fi        
+    fi
+    return 1
 }
 
 
-install_vimrc
+function main()
+{
+    if ! check_vim_executable ; then
+        echo
+        echo "Can not fin vim executable"
+        echo "Please install vim at first"
+        echo
+        exit 1
+    fi
 
-echo "vim setting done"
+    if install_vimrc ; then
+        echo
+        echo "Install vim config success"
+        echo
+        exit 0
+    fi
+
+    echo
+    echo "Install vim config failed"
+    echo
+    exit 1
+}
+
+main
